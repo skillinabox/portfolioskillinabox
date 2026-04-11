e2fd08fae38d8ad78f073fedfdbe5f41d1e2d734
skillinabox <54572299+skillinabox@users.noreply.github.com>
2026-04-11 08:54:57 +0530

    Delete skillinabox-app directory

--- skillinabox-app/src/lib/utils.js
+++
@@ -1,9 +0,0 @@
-// Utility: convert File to base64 string
-export function fileToBase64(file) {
-  return new Promise((resolve, reject) => {
-    const reader = new FileReader()
-    reader.onload = e => resolve(e.target.result.split(',')[1])
-    reader.onerror = reject
-    reader.readAsDataURL(file)
-  })
-}
